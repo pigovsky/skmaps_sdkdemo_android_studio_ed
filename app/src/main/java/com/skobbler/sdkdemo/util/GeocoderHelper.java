def77b649c58c435e3e33e9ec749940a465ae9d9
Yuriy Pigovsky <pigovsky@gmail.com>
2014-08-02 18:47:38 +0300

    Reformatted according to java code conventions

--- app/src/main/java/com/skobbler/sdkdemo/util/GeocoderHelper.java
+++ app/src/main/java/com/skobbler/sdkdemo/util/GeocoderHelper.java
@@ -17,17 +17,14 @@ import org.json.JSONObject;
 
 import java.net.URLEncoder;
 
-public class GeocoderHelper
-{
+public class GeocoderHelper {
     private static final AndroidHttpClient ANDROID_HTTP_CLIENT = AndroidHttpClient.newInstance(GeocoderHelper.class.getName());
 
-    public static String fetchCityNameUsingGoogleMap(SKCoordinate location)
-    {
+    public static String fetchAddressFromLoactionUsingGoogleMap(SKCoordinate location) {
         String googleMapUrl = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + location.getLatitude() + ","
                 + location.getLongitude() + "&sensor=false&language=uk";
 
-        try
-        {
+        try {
             JSONObject googleMapResponse = new JSONObject(ANDROID_HTTP_CLIENT.execute(new HttpGet(googleMapUrl),
                     new BasicResponseHandler()));
 
@@ -35,25 +32,21 @@ public class GeocoderHelper
 
             JSONObject result = results.getJSONObject(0);
 
-            Log.println(Log.INFO, "JSON:",result.toString());
+            Log.println(Log.INFO, "JSON:", result.toString());
 
             String address = result.get("formatted_address").toString();
 
             Log.println(Log.INFO, "JSON:", address);
             return address;
-        }
-        catch (Exception ignored)
-        {
+        } catch (Exception ignored) {
             ignored.printStackTrace();
         }
         return null;
     }
 
-    public static SKCoordinate fetchLocationFromAddressUsingGoogleMap(String address)
-    {
+    public static SKCoordinate fetchLocationFromAddressUsingGoogleMap(String address) {
 
-        try
-        {
+        try {
             String googleMapUrl =
                     String.format("http://maps.googleapis.com/maps/api/geocode/json?address=%s",
                             URLEncoder.encode(address, "utf-8"));
@@ -64,7 +57,7 @@ public class GeocoderHelper
 
             JSONObject result = results.getJSONObject(0);
 
-            Log.println(Log.INFO, "JSON:",result.toString());
+            Log.println(Log.INFO, "JSON:", result.toString());
 
             JSONObject locationJSON = result.getJSONObject("geometry").getJSONObject("location");
 
@@ -73,9 +66,7 @@ public class GeocoderHelper
             location.setLongitude(Double.parseDouble(locationJSON.getString("lng")));
 
             return location;
-        }
-        catch (Exception ignored)
-        {
+        } catch (Exception ignored) {
             ignored.printStackTrace();
         }
         return null;
